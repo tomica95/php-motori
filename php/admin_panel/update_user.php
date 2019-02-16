19426a576fd699d8c7510ea1f47a58c59ea68e86
tomica95 <seleatoma@gmail.com>
2019-02-16 17:19:11 +0100

    validacija za update user-a

--- php/admin_panel/update_user.php
+++ php/admin_panel/update_user.php
@@ -6,21 +6,61 @@
 
     $stmt = $connection->prepare($update_user);
 
+    $username = $_REQUEST['username'];
+
+    $email = $_REQUEST['email'];
+
     $password = $_REQUEST['password'];
 
-    $passwordcript = md5($password);
+   
+
+    //pocetak validacije
+
+    $regUsername = "/[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*/";
+
+   
+
+    $regMail = "/[\w\-.+_%]+@[\w\.\-]+\.[A-Za-z0-9]{2,}/";
 
     
+    $regPassword = "/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}/";
+
+    
+    $greske = [];
+
+    if(!preg_match($regUsername,$username)){
+
+        $greske[]="Username nije okej";
+    }
+    
+    if(!preg_match($regMail,$email)){
+
+        $greske[]="Mail nije okej";
+    }
+    if(!preg_match($regPassword,$password)){
+       
+        $greske[]="Password nije okej";
+    }
+    
+
+    if(count($greske)>0){
+
+        echo "Forma nije popunjena u dobrom formatu";
+    }
+    else
+    {
+        
+     $passwordcript = md5($password);
 
     $stmt->execute([
-        'username'=>$_REQUEST['username'],
+        'username'=>$username,
         'password'=>$passwordcript,
-        'email'=>$_REQUEST['email'],
+        'email'=>$email,
         'id'=>$_REQUEST['user']
 
     ]);
 
     Header('Location:../../index.php?page=adminpanel');
 
-
+    }
 ?>
