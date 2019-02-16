1b3872626311a4f6a03f6abcfe0182844da21f9f
tomica95 <seleatoma@gmail.com>
2019-02-16 17:14:08 +0100

    validacija insert usera

--- php/admin_panel/insert_users.php
+++ php/admin_panel/insert_users.php
@@ -13,24 +13,82 @@
     $input = "INSERT INTO users VAlUES('',:username,:password,:email,:token,:verifikovan,:role)";
 
     $stmt = $connection->prepare($input);
+    
+    //validacija za user-a
+    
+    $regUsername = "/[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*/";
 
-   $proslo =  $stmt->execute([
-        'username'=>$username,
-        'password'=>$password,
-        'email'=>$email,
-        'token'=>'11111',
-        'verifikovan'=>'1',
-        'role'=>$list
-    ]);
+   
 
-    if($proslo){
+    $regMail = "/[\w\-.+_%]+@[\w\.\-]+\.[A-Za-z0-9]{2,}/";
 
-        header('Location:../../index.php?page=adminpanel');
+    
+    $regPassword = "/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}/";
+
+    
+    $greske = [];
+
+    if(!preg_match($regUsername,$username)){
+
+        $greske[]="Username nije okej";
+    }
+    
+    if(!preg_match($regMail,$email)){
+
+        $greske[]="Mail nije okej";
+    }
+    if(!preg_match($regPassword,$password)){
+       
+        $greske[]="Password nije okej";
+    }
+    
+
+    if(count($greske)>0){
+
+        echo "Forma nije popunjena u dobrom formatu";
     }
     else
     {
-        echo "Napravili ste gresku prilikom unosa";
+        
+    //kraj validacije
+
+   $proslo =  $stmt->execute([
+    'username'=>$username,
+    'password'=>$password,
+    'email'=>$email,
+    'token'=>'11111',
+    'verifikovan'=>'1',
+    'role'=>$list
+]);
+
+if($proslo){
+
+    header('Location:../../index.php?page=adminpanel');
+}
+else
+{
+    echo "Napravili ste gresku prilikom unosa";
+}
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
