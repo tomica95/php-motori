a565e1535724de176789b9caf8ffe0a1c73f6084
tomica95 <seleatoma@gmail.com>
2019-02-16 18:51:21 +0100

    sredjen dizajn

--- views/post.php
+++ views/post.php
@@ -5,23 +5,23 @@
     ?>
    
     <!-- Post Content -->
-    <article>
+    <article class="post">
       <div class="container">
         <div class="row">
           <div class="col-lg-8 col-md-10 mx-auto">
           <h1><?= $post->title ?></h1>
           <h4><?= $post->subtitle?></h4>
-          <h3><?= $post->text?></h3>
           <img src="<?= $post->src?>">
-           <p></p>
-            Text by <?= $post->username ?>
+           <p class="text"><?= $post->text?></p>
+            
+            <p class="author"><i class="fas fa-user"></i> Autor: <span><?= $post->username ?></span></p>
              
           </div>
         </div>
       </div>
     </article>
 
-    <hr>
+  
   <?php if(isset($_SESSION['user'])):?>
 
 
@@ -29,19 +29,27 @@
 
 
   <!--Komentari -->
+<div class="komentar-forma container">
+  <div class="row">
+    <div class="col-lg-8 col-md-10 mx-auto">
+      <div class="naslov">
+        <h6>Dodaj komentar</h6>
+      </div>
     <form>
       <input type="hidden" id="vracaPostId" value="<?=$post->id_post?>">
   <div class="form-group">
-  <label for="comment">Naslov:</label>
-    <label class="sr-only" for="title">Naslov</label>
+    <label class="sr-only" for="comment">Ime</label>
     <input type="text" class="form-control" name="title" id="title">
   </div>
   <div class="form-group">
-  <label for="comment">Komentar:</label>
+  <label for="comment">Komentar</label>
   <textarea class="form-control" rows="5" name="comment" id="comment"></textarea>
 </div>
   <button type="button" id="unesi-komentar"name="submit" class="btn btn-default">Submit</button>
 </form>
+    </div>
+  </div>
+  </div>
 
 <div id="primac-komentara">
 
@@ -49,11 +57,30 @@
 
 <?php endif; ?>
 <?php include "php/comments.php" ?>
+  <div class="komentari container">
+  <div class="row">
+    <div class="col-lg-8 col-md-10 mx-auto">
+      <div class="naslov">
+        <h6>Komentari</h6>
+      </div>
+    </div>
+    </div>
+  </div>
 <?php foreach($comments as $comment):?>
-
-  Title:<?=$comment->title?></br>
-  Body:<?=$comment->body?></br>
-  Post napisao:<?=$comment->username?></br>
+  
+  <div class="komentari container">
+  <div class="row">
+    <div class="col-lg-8 col-md-10 mx-auto">
+      
+      <div class="item">
+        <p class="naslov"><?=$comment->title?></p>
+        <p class="tekst"><?=$comment->body?></p>
+        <p class="author"><i class="fas fa-user"></i> Autor: <span><?=$comment->username?></span></p>
+      </div>
+      
+    </div>
+    </div>
+  </div>
 
 
 <?php endforeach; ?>
